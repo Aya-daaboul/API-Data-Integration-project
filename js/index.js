7951739ebc1c956e49f1c6da805691491c48bb4c
Aya <ayadaaboul02@outlook.com>
2025-01-02 16:50:07 +0200

    updated html and js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 //https://jsonplaceholder.typicode.com/users
+getUsers()
+async function getUsers() {
+    try{
+        const response= await fetch("https://jsonplaceholder.typicode.com/users");
+        if (!response.ok){
+            throw new Error("Unable to fetch the provided resource");
+        }
+        const data = await response.json();
+        console.log(data);
+        for(i=0;i<10;i++){
+            if(!data[i].name){
+                document.querySelector(`..name--${i}`).innerHTML="N/A";   
+            }
+            else{
+                document.querySelector(`.name--${i}`).innerHTML=data[i].name;
+            }
+            if(!data[i].username)
+            {
+                document.querySelector(`.username--${i}`).innerHTML="N/A";
+            }
+            else{
+                document.querySelector(`.username--${i}`).innerHTML=data[i].username;
+            }
+            if(!data[i].email){
+                document.querySelector(`.email--${i}`).innerHTML="N/A";
+            }
+            else{
+            document.querySelector(`.email--${i}`).innerHTML=data[i].email;
+    }
+    }
+}
+    catch(error){
+        console.log(error);
+
+    }
+}
